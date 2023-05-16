92451256d153d82ff3148c44b3d00da7ff56d138
Oskar <oskar.johannes.laht@tktk.ee>
2023-05-16 17:39:52 +0300

    Week7

---
+++ Calculator/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Enter the operation (+/-): ");
+char userOperator = Char.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter value a: ");
+int firstNum = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter value b: ");
+int secondNum = Int32.Parse(Console.ReadLine()); 
+
+switch (userOperator)
+{
+    case '+':
+        Addition(firstNum, secondNum);
+        break;
+
+    case '-':
+        Subtraction(firstNum, secondNum);
+
+    break;
+        default: Console.WriteLine("Error");
+        break;
+
+
+}
+static void Addition(int a, int b)
+{
+    Console.WriteLine($"{a} + {b} = {a + b}");
+
+}
+
+static void Subtraction(int a, int b)
+{
+    Console.WriteLine($"{a} - {b} = {a - b}");
+
+}
---
+++ DiscountCalculator/Program.cs
@@ -0,0 +1,40 @@
+﻿
+// < 10 - 1%
+//total >= 10 && < 20 - 5%
+//total >= 20 - 10%
+
+
+Console.WriteLine("Provide total: ");
+int userInput = Int32.Parse(Console.ReadLine());    
+
+double discount = CalculatorDiscount(userInput);
+
+Console.WriteLine($"Your discount is: {discount} %");
+
+double newTotal = CalculateNewTotal(userInput, discount);
+
+Console.WriteLine(newTotal);
+
+
+ static double CalculatorDiscount(int total)
+{
+    if(total < 10)
+    {
+        return 1;
+    }
+    else if(total >= 10 && total < 20) 
+    {
+        return 5;
+    }
+    else 
+    { 
+        return 10;
+    }
+
+}
+
+static double CalculateNewTotal(double total, double discount)
+{
+    double result = total - (newTotal * discount) / 100;
+    return result;
+}
